d8e4e1c5c7621807cf2e34d7c7765a0fdacecb27
Phillip Davis <phdavis1027@gmail.com>
2021-07-30 09:00:12 -0400

    table resize almost working, slight stylesheet cleanup

--- main.js
+++ main.js
@@ -15,13 +15,16 @@ function initializeTable(){
     for(let i = 1; i<=dims; i++){
         let row = document.createElement("div")
         row.classList.add("row");
+        row.setAttribute("id", "row-" + i.toString())
+        row.style.height = (100/dims).toString() + "%"
+        row.style.width = "100%"
         table.appendChild(row);
         for(let j = 1; j<= dims;j++){
             let cell = document.createElement("div");
             cell.classList.add("cell");
             cell.classList.add("empty");
             cell.style.width = dims + "%";
-            cell.style.height = "100%";
+            cell.setAttribute("id", "cell-" + j.toString())
             row.appendChild(cell);
         }
     }  
@@ -59,6 +62,10 @@ function addSliderListeners(){
 
         s.addEventListener("mousedown", ()=>{
             mouseIsDown = true;
+            let size = parseInt(s.getAttribute("id"))
+            dims = parseInt(size)
+            updateSlider(size, selectorDivs);
+            updateTable()
         })
 
         s.addEventListener("mouseup", ()=>{
@@ -91,6 +98,7 @@ function updateSlider(size, selectorDivs){
 
 function updateTable(){
     clearTable();
+    adjustDims();
 }
 
 function clearTable(){
@@ -98,8 +106,65 @@ function clearTable(){
     rows.forEach((r)=>{
         let cells = r.querySelectorAll(".cell")
         cells.forEach((c)=>{
-            c.style.backgroundColor="grey"
+            c.classList.add("empty")
         })
     })
+}
 
+function adjustDims(){
+    let oldDims = document.querySelectorAll('.row').length 
+    if(oldDims > dims){
+        for (let oldRow = oldDims; oldRow > dims; oldRow--){ //get rid of extraneous rows
+            let row = table.querySelector("#row-" + oldRow)
+            table.removeChild(row)
+        }
+
+
+        for (let rows = 1; rows <= dims; rows++){//resize old rows
+            let row = table.querySelector("#row-"+rows)
+            row.style.height = (100/dims).toString() + "%"
+            for(let cells = 1; cells <= oldDims; cells++){
+                let cell = row.querySelector("#cell-"+cells)
+                if(cells <= dims){
+                    cell.style.width = (100 / dims).toString() + "%"
+                }
+                else{
+                    row.removeChild(cell)
+                }
+            }
+        }
+    }
+    if(oldDims < dims){
+        for(let newRow = oldDims + 1; newRow <= dims; newRow++){ //add new rows
+            let row = document.createElement("div");
+            row.setAttribute("id", "row-" + newRow.toString())
+            row.classList.add("row")
+            row.style.height = (100 / dims).toString() + "%"
+            table.appendChild(row)
+            for (let newCell = 1; newCell <= dims; newCell++){
+                let cell = document.createElement("div");
+                cell.classList.add("cell");
+                cell.classList.add("empty");
+                cell.style.width = (100 / dims) + "%";
+                cell.setAttribute("id", "cell-" + newCell.toString())
+                row.appendChild(cell);
+            }
+        }
+        for (let rows = 1; rows <= oldDims; rows++){//resize old rows
+            let row = table.querySelector("#row-"+rows)
+            row.style.height = (100/dims).toString() + "%"
+            for(let cells = 1; cells <= oldDims; cells++){ //resize old cells
+                let cell = row.querySelector("#cell-" + cells)
+                cell.style.width = (100/dims).toString() + "%"
+            }
+            for(let newCells = oldDims + 1; newCells <= dims; newCells++){ //inject new cells into old rows
+                let cell = document.createElement("div")
+                cell.classList.add("cell")
+                cell.classList.add("empty")
+                cell.style.width = (100 / dims) + "%";
+                cell.setAttribute("id", "cell-" + newCells.toString())
+                row.appendChild(cell)
+            }
+        }
+    }
 }
